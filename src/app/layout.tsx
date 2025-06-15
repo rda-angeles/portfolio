c99be13a5550acc08defb50bd8ad1e14d73d907b
rda-angeles <raedominique.angeles@gmail.com>
2025-06-16 00:14:39 +0800

    chore: added metadata title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,18 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Raleway } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { Metadata } from "next";
 
 const raleway = Raleway({
   subsets: ["latin"],
   weight: ["100", "500", "200"],
   fallback: ["Arial", "sans-serif"],
 });
+
+export const metadata: Metadata = {
+    title: 'RDA | Portfolio',
+    description: "A very informative website about me!"
+};
 
 export default function RootLayout({
   children,
